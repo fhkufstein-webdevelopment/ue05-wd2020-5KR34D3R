5fd93b6f357b450a3775bdb68d03a5fd5ac6abd9
Trebo Georg <1910653989@stud.fh-kufstein.ac.at>
2020-03-19 04:20:32 +0100

    Behebung des fehler, dass keine Daten hinzugefuegt werden konnten bzw geloescht

--- js/user.js
+++ js/user.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
 
-    var userListBody = $('.userList tbody');
+    var userListBody = $('.userList tbody')[0];
 
     //@todo store and somehow update the current number of users
+
+
 
 
     $('.needs-validation').submit(function(event) {
@@ -26,20 +28,44 @@ $(document).ready(function() {
         //6. maybe do something else... :-)
 
         //your code follows here
+        let newRow = userListBody.insertRow();
+        let inputValue = document.getElementById("username");
 
+        let cell1 = newRow.insertCell(0);
+        let cell2 = newRow.insertCell(1);
+        let cell3 = newRow.insertCell(2);
+
+        let txtNodeCount = document.createTextNode(userListBody.rows.length);
+        let txtNodeName = document.createTextNode(inputValue.value);
+        let button = document.createElement("button");
+        let buttonI = document.createElement("i");
+
+        button.type = "button";
+
+
+        button.classList.add('btn', 'btn-secondary', 'btn-danger', 'deleteTrigger');
+        button.title = "Löschen";
+        buttonI.classList.add('fa', 'fa-trash');
+        button.appendChild(buttonI);
+
+        cell1.appendChild(txtNodeCount);
+        cell2.appendChild(txtNodeName);
+        cell3.appendChild(button);
+        inputValue.value = "";
         return false;
+        $('.btn').load('.btn');
     });
 
-
-    $('.deleteTrigger').click(function() {
-        //@todo
-        //1. remove current user from dom
-        //2. update number of current users
-
-        //your code follows here
-
-
+    $(document).on('click', '.btn-danger',function (event) {
+        var loeschneJaNein = confirm('Moechen Sie den Datensatz loeschen?');
+        if(loeschneJaNein == true){
+            ($(event.target).closest("tr")).remove();
+            for(let i = 0,row;row = userListBody.rows[i]; i++){
+                row.cells[0].innerHTML = i+1;
+            }
+        }
     });
+
 
     //maybe some code follows here
 
